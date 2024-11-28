afc34b55400837500a7b3dcac2750e578399d1b4
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-11-28 15:14:50 +0900

    edit post screen

--- lib/screen/post_screen.dart
+++ lib/screen/post_screen.dart
@@ -21,7 +21,7 @@ class _PostScreenState extends State<PostScreen> {
         child: Stack(
           children: [
             Padding(
-              padding: const EdgeInsets.only(top: 20, right: 20),
+              padding: const EdgeInsets.only(right: 20),
               child: Align(
                 alignment: Alignment.topRight,
                 child: IconButton(
