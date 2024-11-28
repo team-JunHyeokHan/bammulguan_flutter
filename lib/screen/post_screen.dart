4fd9cf085211217df68b4ad619f153358cba128c
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-11-28 10:27:07 +0900

    create title content screen

--- lib/screen/post_screen.dart
+++ lib/screen/post_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 
@@ -12,10 +11,99 @@ class PostScreen extends StatefulWidget {
 class _PostScreenState extends State<PostScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(onPressed: (){
-        Navigator.pop(context);
-      }, child: Text("back")),
+    final textTheme = Theme.of(context).textTheme;
+    final titleController = TextEditingController();
+    final contentController = TextEditingController();
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20, right: 20),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.close_outlined,
+                    color: Colors.white,
+                    size: 36,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 100, 20, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    minLines: 1,
+                    maxLines: 1,
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "제목을 입력해주세요",
+                      hintStyle: textTheme.headlineSmall,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: 60,
+                    height: 3,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    child: TextField(
+                      minLines: 1,
+                      maxLines: 10,
+                      controller: contentController,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.white),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.white),
+                        ),
+                        hintText: "당신의 기억을 작성해 보세요.",
+                        hintStyle: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print(titleController.text);
+                    print(contentController.text);
+                  },
+                  child: Text("다음"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    textStyle: textTheme.titleMedium,
+                    minimumSize: Size(MediaQuery.of(context).size.width, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
